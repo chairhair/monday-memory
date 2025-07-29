f044a5d73d10fa4aa6017c28d5b9aea42b379e69
Joe Brock <joebrockprof@gmail.com>
2025-07-29 00:06:58 -0400

    Implementing Repositories for later use in Verification controller

---
+++ src/main/java/com/monday/monday_backend/auth/roles/AccessLevel.java
@@ -0,0 +1,12 @@
+package com.monday.monday_backend.auth.roles;
+
+public enum AccessLevel {
+    INTERNAL_SERVICE,
+    READ_ONLY,
+    WRITE,
+    ADMIN,
+    SCHEDULER,
+    ANALYTICS,
+    EXTERNAL_CLIENT,
+    SYSTEM_MAINTAINER,
+}
--- src/main/java/com/monday/monday_backend/auth/roles/RolesEntity.java
+++ src/main/java/com/monday/monday_backend/auth/roles/RolesEntity.java
@@ -1,12 +1,20 @@
 package com.monday.monday_backend.auth.roles;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.Instant;
 
 @Entity
 public class RolesEntity {
     @Id
     private long id;
 
+    @Enumerated(EnumType.STRING)
+    private AccessLevel accessLevel;
 
+    private Instant expiration;
+
+    @ManyToOne
+    @JoinColumn(name = "token_id")
+    private String token;
 }
