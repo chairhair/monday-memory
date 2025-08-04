18b5386d9e48e2e588eeb0e439fbe274d463d1d3
Joe Brock <joebrockprof@gmail.com>
2025-08-04 00:36:10 -0400

    08/04/2025: Query Guest and User controllers have been created.

---
+++ src/main/java/com/monday/monday_backend/query/guest/QueryGuestController.java
@@ -0,0 +1,11 @@
+package com.monday.monday_backend.query.guest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/query/guest")
+public class QueryGuestController {
+}
---
+++ src/main/java/com/monday/monday_backend/query/user/QueryUserController.java
@@ -0,0 +1,11 @@
+package com.monday.monday_backend.query.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/query/user")
+public class QueryUserController {
+}
