de6a51b4c95b3f7e319dfe73e15b4553cf64957f
Joe Brock <joebrockprof@gmail.com>
2025-08-07 00:17:57 -0400

    08/07/2025: Adding swagger annotations

--- src/main/java/com/monday/monday_backend/query/memory/SessionMemoryController.java
+++ src/main/java/com/monday/monday_backend/query/memory/SessionMemoryController.java
@@ -1,13 +1,11 @@
 package com.monday.monday_backend.query.memory;
 
-import com.monday.monday_backend.query.memory.dto.MemoryChunkDTO;
 import com.monday.monday_backend.query.memory.dto.SessionMemoryFilterRequestDTO;
 import com.monday.monday_backend.query.memory.dto.SessionMemoryRequestDTO;
 import com.monday.monday_backend.query.memory.dto.SessionMemoryResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,13 +17,13 @@ public class SessionMemoryController {
 
     @PostMapping
     public SessionMemoryResponseDTO createMemory(@RequestBody SessionMemoryRequestDTO memoryRequestDTO){
-        return new SessionMemoryResponseDTO(null, null, null, null);
+        return new SessionMemoryResponseDTO(null, null, null, null, null);
     }
 
     @PostMapping
     public SessionMemoryResponseDTO getMemoryList(
             @RequestBody SessionMemoryFilterRequestDTO sessionMemoryFilterRequestDTO) {
-        return new SessionMemoryResponseDTO(null, null, null, null);
+        return new SessionMemoryResponseDTO(null, null, null, null, null);
     }
 
     @DeleteMapping
