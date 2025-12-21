f23785f0b5333e8b23fe8efd2f708d98f99610a6
chairhair <joebrock3@yahoo.com>
2025-12-20 23:15:15 -0500

    12/20/2025: House cleaning on DB migration and flyway

--- src/main/java/com/monday/monday_backend/options/OptionsRepository.java
+++ src/main/java/com/monday/monday_backend/options/OptionsRepository.java
@@ -3,6 +3,8 @@ package com.monday.monday_backend.options;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface OptionsRepository extends JpaRepository<OptionsEntity, UUID> {
 }
