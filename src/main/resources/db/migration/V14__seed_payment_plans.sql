9a284a77a3673d95bcd306c53bf244c1a0757be2
chairhair <joebrock3@yahoo.com>
2025-12-27 00:44:15 -0500

    12/27/2025: Worked a little bit more on the payments. I'm getting there. Just need to handle emails a little better

--- src/main/resources/db/migration/V14__seed_payment_plans.sql
+++ src/main/resources/db/migration/V14__seed_payment_plans.sql
@@ -26,9 +26,9 @@ INSERT INTO price_plan (
 (
     gen_random_uuid(),
     'PRO_MONTHLY',
-    'prod_TKPkiHgGMSfRzE',       -- replace with real Stripe price ID
+    'price_1SNkvIDSiKSuaYLRynJ5wulg',       -- replace with real Stripe price ID
     'Pro (Monthly)',
-    100,                          -- $10.00
+    1000,                          -- $10.00
     NULL,
     200,
     1000000,
@@ -39,7 +39,7 @@ INSERT INTO price_plan (
 (
     gen_random_uuid(),
     'PRO_ANNUAL',
-    'prod_TKPkiHgGMSfRzE',        -- replace with real Stripe price ID
+    'price_1SNkvqDSiKSuaYLRQ0UoIfZx',        -- replace with real Stripe price ID
     'Pro (Annual)',
     NULL,
     10000,                         -- $100.00 ($8.33/mo)
