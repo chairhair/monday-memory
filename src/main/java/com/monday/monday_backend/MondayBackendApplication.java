05b0fd5a385b088acb4a57c410a9e9b389d3c85a
chairhair <joebrock3@yahoo.com>
2026-01-18 22:26:13 -0500

    01/18/2026: Ensuring that we're only targeting that within our Monday BE

--- src/main/java/com/monday/monday_backend/MondayBackendApplication.java
+++ src/main/java/com/monday/monday_backend/MondayBackendApplication.java
@@ -2,10 +2,12 @@ package com.monday.monday_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "com.monday.monday_backend")
 public class MondayBackendApplication {
 
 	public static void main(String[] args) {
