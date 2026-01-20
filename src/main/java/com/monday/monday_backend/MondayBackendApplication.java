5b13c4c932d7ab9acc49c0b643b0df7e14b92cbc
chairhair <joebrock3@yahoo.com>
2026-01-19 21:05:14 -0500

    01/19/2026: Removed redis from package for build.

--- src/main/java/com/monday/monday_backend/MondayBackendApplication.java
+++ src/main/java/com/monday/monday_backend/MondayBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaRepositories(basePackages = "com.monday.monday_backend")
 public class MondayBackendApplication {
 
 	public static void main(String[] args) {
