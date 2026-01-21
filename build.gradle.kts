9a60dddb73bff2aab7cc872cd5a067a8bbba517b
chairhair <joebrock3@yahoo.com>
2026-01-20 22:32:46 -0500

    01/20/2026: Updated to grab latest shared monday mem code

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ configurations {
 }
 
 dependencies {
-	implementation("com.monday:shared-monday-memory-be-library:0.1.4")
+	implementation(project(":shared-monday-memory-be-library"))
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
